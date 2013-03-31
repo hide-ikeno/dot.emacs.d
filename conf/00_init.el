39919c1c5b30249b3bd97a5d82d14057e85bc382
Hidekazu Ikeno <hide.ikeno@gmail.com>
2013-04-01 01:55:22 +0900

    revert changes in conf/00_init.el

--- conf/00_init.el
+++ conf/00_init.el
@@ -84,6 +84,32 @@
     (ruler-mode t))
 
 ;;==============================================================================
+;; Customize some default keybinds
+;;==============================================================================
+
+;; C-h: BackSpace (<= help-for-help)
+(keyboard-translate ?\C-h ?\C-?)
+
+;; C-m newline-and-indent (<= newline)
+(global-set-key (kbd "C-m") 'newline-and-indent)
+;; C-t other-window (<= transpose-chars)
+(global-set-key (kbd "C-t") 'other-window)
+
+;; M-?, [f1]: help-for-help
+(global-set-key (kbd "M-?") 'help-for-help)
+(global-set-key [f1] 'help-for-help)
+
+;; M-h backward-kill-word (<= mark-paragraph)
+(global-set-key (kbd "M-h") 'backward-kill-word)
+;; M-H mark-paragraph
+(global-set-key (kbd "M-H") 'mark-paragraph)
+;; M-g goto-line
+(global-set-key (kbd "M-g") 'goto-line)
+
+;;; 日本語でインクリメンタルサーチ
+(define-key isearch-mode-map (kbd "C-k") 'isearch-edit-string)
+
+;;==============================================================================
 ;; Customize basic packages
 ;;==============================================================================
 
