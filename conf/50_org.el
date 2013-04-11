9f18d09b00e0897a6777d24647f38f4b0257c096
Hidekazu Ikeno <hide.ikeno@gmail.com>
2013-04-11 19:22:25 +0900

    Org-mode -- change org directories

--- conf/50_org.el
+++ conf/50_org.el
@@ -15,13 +15,12 @@
   (global-set-key "\C-cb" 'org-iswitchb)
 
   ;; Default directory for Org files
-  (setq org-directory "~/Dropbox/org")
+  ;; (setq org-directory "~/Dropbox/org")
 
   ;; Agenda
-  ;; (setq org-agenda-files (list (concat org-directory "/todo.org")
-  ;;                              (concat org-directory "/birthday.org")))
-  (setq org-agenda-files (list org-directory
-                               (concat org-directory "/research")))
+  (setq org-agenda-files (list "~/Dropbox/org"
+                               "~/Dropbox/org/research"
+                               "~/org"))
 
   ;; Task (TODO) list
   ;; "C-c C-t" -> org-todo
@@ -61,45 +60,22 @@
   ;; Task 完了時の時刻を記録
   (setq org-log-done 'time)
 
-  ;; Capture templates
-  ;; (setq org-capture-templates
-  ;;       '(
-  ;;         ;; TODO list
-  ;;         ("t" "TODO" entry (file+headline "~/Dropbox/org/todo.org" "Tasks")
-  ;;          "* TASK %?n %in %a")
-  ;;         ;; Shopping
-  ;;         ("s" "Shopping" entry (file+headline "~/Dropbox/org/todo.org" "Shopping")
-  ;;          "* TASK %?n %in %a")
-  ;;         ;; Memo, Bookmarks, records
-  ;;         ("j" "Journal" entry (file+datetree "~/Dropbox/org/journal.org")
-  ;;          "* %?n %Un %in %a")
-  ;;         ;; Research note
-  ;;         ("n" "Note" entry (file+headline "~/Dropbox/org/notes.org" "Notes")
-  ;;          "* %?n %Un %i")
-  ;;         ;; Literature in BiBTeX
-  ;;         ("a" "Article" entry (file+headline "~/Dropbox/org/reading.org" "Articles")
-  ;;          "* %An Ref. on %Un %:author [%:year]: %:titlen
-  ;;                                  In %:journal, pp. %:pages.n Comment: %?")
-  ;;         ("b" "Book" entry (file+headline "~/Dropbox/org/reading.org" "Books")
-  ;;          "* %An Ref. on %Un %:author [%:year]: %:titlen Comment: %?")
-  ;;         ))
-
   ;; Capture templates for: TODO tasks, Notes, appointments, phone calls, and
   ;; org-protocol
   (setq org-capture-templates
-        '(("t" "todo" entry (file (concat org-directory "/refile.org"))
+        '(("t" "todo" entry (file "~/Dropbox/org/refile.org")
            "* TASK %?\n%U\n%a\n" :clock-in t :clock-resume t)
-          ("r" "respond" entry (file (concat org-directory "/refile.org"))
+          ("r" "respond" entry (file "~/Dropbox/org/refile.org")
            "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
-          ("n" "note" entry (file (concat org-directory "/refile.org"))
+          ("n" "note" entry (file "~/Dropbox/org/refile.org")
            "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
-          ("j" "Journal" entry (file+datetree (concat org-directory "/journal.org"))
+          ("j" "Journal" entry (file+datetree  "~/Dropbox/org/journal.org")
            "* %?\n%U\n" :clock-in t :clock-resume t)
-          ("w" "org-protocol" entry (file (concat org-directory "/refile.org"))
+          ("w" "org-protocol" entry (file "~/Dropbox/org/refile.org")
            "* TODO Review %c\n%U\n" :immediate-finish t)
-          ("p" "Phone call" entry (file (concat org-directory "/refile.org"))
+          ("p" "Phone call" entry (file "~/Dropbox/org/refile.org")
            "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
-          ("h" "Habit" entry (file (concat org-directory "/refile.org"))
+          ("h" "Habit" entry (file "~/Dropbox/org/refile.org")
            "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"<%Y-%m-%d %a .+1d/3d>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n")))
 
   ;; Remove empty LOGBOOK drawers on clock out
@@ -111,15 +87,16 @@
 
   (add-hook 'org-clock-out-hook 'bh/remove-empty-drawer-on-clock-out 'append)
 
-  ;; Refiling configurations
-  ;; Targets include this file and any file contributing to the agenda - up to 9 levels deep
+  ;;; Refiling configurations
+  ;; Targets include this file and any file contributing to the agenda - up to 9
+  ;; levels deep
   (setq org-refile-targets '((nil :maxlevel . 9)
                              (org-agenda-files :maxlevel . 9)))
 
   ;; Allow refile to create parent tasks with confirmation
   (setq org-refile-allow-creating-parent-nodes 'confirm)
 
-  ;;;; Refile settings
+  ;;; Refile settings
   ;; Exclude DONE state tasks from refile targets
   (defun bh/verify-refile-target ()
     "Exclude todo keywords with a done state from refile targets"
