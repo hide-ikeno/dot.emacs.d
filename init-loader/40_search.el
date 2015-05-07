a11688148715caf6aa90e830e72f243a4b2ffe7c
hide-ikeno <hide.ikeno@gmail.com>
2015-05-07 12:26:12 +0900

    Merge 40_grep.el and 40_imenus.el into 40_search.el

--- init-loader/40_search.el
+++ init-loader/40_search.el
@@ -87,3 +87,103 @@
 (use-package isearch-dabbrev
   :init (bind-key "TAB" 'isearch-dabbrev-expand isearch-mode-map)
   )
+
+
+;;;=============================================================================
+;;; imenus.el --- Imenu for multiple buffers
+;;;=============================================================================
+(el-get-bundle imenus)
+;; See, http://rubikitch.com/2015/04/09/imenus/
+(use-package imenus
+  :config
+  ;; エラー対策
+  (defun imenu-find-default--or-current-symbol (&rest them)
+    (condition-case nil
+        (apply them)
+      (error (thing-at-point 'symbol))))
+  (advice-add 'imenu-find-default :around 'imenu-find-default--or-current-symbol)
+  ;; なぜか現在のシンボルを取ってくれないから
+  (defun imenus-exit-minibuffer ()
+    (exit-minibuffer))
+
+  ;; ido化: imenus/with-ido imenus-mode-buffers/with-idoを定義
+  (with-ido-completion imenus)
+  ;; C-M-s C-M-sで現在のシンボルを helm-multi-swoop できるよ！
+  (bind-key "C-M-s" (with-ido-completion imenus-mode-buffers))
+
+  ;; M-oでのmulti-occurをシンボル正規表現にするよう改良
+  (push '(occur . imenus-ido-multi-occur) imenus-actions)
+  (defun imenus-ido-multi-occur (buffers input)
+    (multi-occur buffers
+                 (format "\\_<%s\\_>"
+                         (regexp-quote (replace-regexp-in-string "^.*|" "" input)))))
+
+  ;; C-M-sで関数呼び出しを helm-multi-swoop できるようにした
+  (use-package helm-swoop
+    :defines
+    imenus-actions
+    imenus-minibuffer-map
+    imenus-exit-status
+    :functions
+    imenus-exit-minibuffer
+    :config
+    (push '(helm-multi-swoop . imenus-helm-multi-swoop) imenus-actions)
+    (defun imenus-helm-multi-swoop (buffers input)
+      (helm-multi-swoop (replace-regexp-in-string "^.*|" "" input)
+                        (mapcar 'buffer-name buffers)))
+    (bind-key "C-M-s" 'imenus-exit-to-helm-multi-swoop imenus-minibuffer-map)
+    (defun imenus-exit-to-helm-multi-swoop ()
+      "Exit from imenu prompt; start `helm-multi-swoop' with the current input."
+      (interactive)
+      (setq imenus-exit-status 'helm-multi-swoop)
+      (imenus-exit-minibuffer))
+    )
+  )
+
+;;;=============================================================================
+;;; helm-ag
+;;;=============================================================================
+(el-get-bundle helm-ag)
+(use-package helm-ag
+  ;; C-M-g -> helm-ag
+  ;; C-M-k -> backward-kill-sexp (remap)
+  :bind (("C-M-g" . helm-ag)
+         ("C-M-k" . backward-kill-sexp))
+  :config
+  ;; 現在のシンボルをデフォルトのクエリにする
+  (setq helm-ag-insert-at-point 'symbol)
+  (defun helm-ag-dot-emacs ()
+     ".emacs.d以下を検索"
+     (interactive)
+     (helm-ag "~/.emacs.d/"))
+  (require 'projectile nil t)
+  (defun helm-projectile-ag ()
+     "Projectileと連携"
+     (interactive)
+     (helm-ag (projectile-project-root)))
+  )
+
+;;;=============================================================================
+;;; wgrep --- Writable grep buffer and apply the changes to files
+;;; wgrep-helm --- helm interface of wgrep
+;;;=============================================================================
+;; C-c C-p : Edit *grep* buffer (default)
+;; C-c C-e : Apply the changes to file buffers.
+;; C-c C-u : All changes are unmarked and ignored.
+;; C-c C-d : Mark as delete to current line (including newline).
+;; C-c C-r : Remove the changes in the region (these changes are not
+;;           applied to the files. Of course, the remaining
+;;           changes can still be applied to the files.)
+;; C-c C-p : Toggle read-only area.
+;; C-c C-k : Discard all changes and exit.
+;; C-x C-q : Exit wgrep mode.
+(use-package wgrep-helm
+  :config
+  ;; r で wgrepモードにする (default "C-c C-p")
+  (setf wgrep-enable-key "r")
+  ;; wgrep終了時にバッファを保存
+  (setq wgrep-auto-save-buffer t)
+  ;; read-only bufferにも変更を適用する
+  (setq wgrep-change-readonly-file t)
+  )
+
