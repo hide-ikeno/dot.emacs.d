d2cd9104fbe8c50e67d075be7036e1f0dafa8123
hide-ikeno <hide.ikeno@gmail.com>
2015-05-19 11:39:26 +0900

    Use clang-format instead of uncrustify

---
+++ init-loader/51_clang-format.el
@@ -0,0 +1,16 @@
+;;; -*- mode: lisp-interaction; coding: utf-8-unix; indent-tabs-mode: nil; -*-
+;;;
+;;; File: 51_clang-format.el
+;;; Description: Customize uncrustify
+;;;
+;;; Code:
+
+;;;=============================================================================
+;;; Emacs frontend of the Uncrustify code beautifier
+;;;=============================================================================
+(el-get-bundle clang-format)
+(use-package clang-format
+  :config
+  (bind-key "C-M-\\" 'clang-format-region c++-mode-map)
+  )
+
