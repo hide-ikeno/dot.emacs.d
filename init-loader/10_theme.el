7b0e206e47bbe743c0fb3446d387a69621a19a06
hide-ikeno <hide.ikeno@gmail.com>
2016-03-02 19:37:42 +0900

    Clean up 10_themes.el

--- init-loader/10_theme.el
+++ init-loader/10_theme.el
@@ -4,37 +4,22 @@
 ;;; Description: Customize Emacs themes
 ;;;
 
-(el-get-bundle atom-one-dark-theme)
-(el-get-bundle birds-of-paradise-plus-theme)
-(el-get-bundle color-theme-zenburn)
-(el-get-bundle darkmine-theme)
-(el-get-bundle dream-theme)
-(el-get-bundle monokai-theme)
-(el-get-bundle material-theme)
-(el-get-bundle solarized-emacs)
-(el-get-bundle tomorrow-theme)
-(el-get-bundle ujelly-theme)
 
-
+;;;=============================================================================
 ;;; Built-in themes
+;;;=============================================================================
 ;; (load-theme 'misterioso t)
 ;; (load-theme 'manoj-dark t)
 ;; (load-theme 'tango-dark t)
 ;; (load-theme 'tsdh-dark t)
 ;; (load-theme 'wombat t)
 
+;;;=============================================================================
 ;;; Third-party themes
-;; (use-package atom-one-dark-theme)
-;; (load-theme 'atom-one-dark t)
-;; (load-theme 'birds-of-paradise-plus t)
-;; (load-theme 'dream t)
-;; (load-theme 'monokai t)
+;;;=============================================================================
+(el-get-bundle material-theme)
+(el-get-bundle solarized-emacs)
+
 (use-package material-theme
   :config
   (load-theme 'material t))
-;; (load-theme 'tomorrow-night-eighties t)
-;; (load-theme 'tomorrow-night-blue t)
-;; (load-theme 'tomorrow-night-bright t)
-;; (load-theme 'tomorrow-night t)
-;; (load-theme 'ujelly t)
-;; (load-theme 'zenburn t)
