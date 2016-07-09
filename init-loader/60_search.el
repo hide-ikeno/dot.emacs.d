1a0e7ec5017bcfb10738779a819f81cda65e40e2
Hidekazu Ikeno <hide.ikeno@gmail.com>
2016-07-10 04:09:03 +0900

    Split wgrep configuration in a separate file

--- init-loader/60_search.el
+++ init-loader/60_search.el
@@ -41,32 +41,3 @@
   ;; eg, M-s-x will activate (avy-goto-word-1 ?x), ie, all words starting with x
   (loop for c from ?! to ?~ do (add-keys-to-avy "M-s-S-" c 'word))
   )
-
-;;;=============================================================================
-;;; wgrep --- Writable grep buffer and apply the changes to files
-;;;=============================================================================
-;; C-c C-p : Edit *grep* buffer (default)
-;; C-c C-e : Apply the changes to file buffers.
-;; C-c C-u : All changes are unmarked and ignored.
-;; C-c C-d : Mark as delete to current line (including newline).
-;; C-c C-r : Remove the changes in the region (these changes are not
-;;           applied to the files. Of course, the remaining
-;;           changes can still be applied to the files.)
-;; C-c C-p : Toggle read-only area.
-;; C-c C-k : Discard all changes and exit.
-;; C-x C-q : Exit wgrep mode.
-;; (el-get-bundle ag)
-(el-get-bundle wgrep)
-(use-package wgrep-ag
-  :defines
-  wgrep-enable-key
-  wgrep-auto-save-buffer
-  wgrep-change-readonly-file
-  :config
-  ;; r で wgrepモードにする (default "C-c C-p")
-  (setf wgrep-enable-key "r")
-  ;; wgrep終了時にバッファを保存
-  (setq wgrep-auto-save-buffer t)
-  ;; read-only bufferにも変更を適用する
-  (setq wgrep-change-readonly-file t)
-  )
